7f341b28ef652c4fa93236ddd1da6e8c40a77631
cqyhahaha <chen458438184@qq.com>
2020-07-28 14:06:22 +0800

    fixed docker account name

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t cqyhahaha/multi-client:latest -t cqyhahaha/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t cqyhahaha/multi-server:latest -t cqyhahaha/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t cqyhahaha/multi-worker:latest -t cqyhahaha/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t cqy186cqy/multi-client:latest -t cqy186cqy/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t cqy186cqy/multi-server:latest -t cqy186cqy/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t cqy186cqy/multi-worker:latest -t cqy186cqy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push cqyhahaha/multi-client:latest
-docker push cqyhahaha/multi-server:latest
-docker push cqyhahaha/multi-worker:latest
+docker push cqy186cqy/multi-client:latest
+docker push cqy186cqy/multi-server:latest
+docker push cqy186cqy/multi-worker:latest
 
-docker push cqyhahaha/multi-client:$SHA
-docker push cqyhahaha/multi-server:$SHA
-docker push cqyhahaha/multi-worker:$SHA
+docker push cqy186cqy/multi-client:$SHA
+docker push cqy186cqy/multi-server:$SHA
+docker push cqy186cqy/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cqyhahaha/multi-server:$SHA
-kubectl set image deployments/client-deployment client=cqyhahaha/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=cqyhahaha/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=cqy186cqy/multi-server:$SHA
+kubectl set image deployments/client-deployment client=cqy186cqy/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=cqy186cqy/multi-worker:$SHA
